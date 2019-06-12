4db1aac0914fb8162a85301d52ae7350ba68519c
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-12 22:50:32 +0100

    validate proptypes for users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
 
 const Users = ({ users, loading }) => {
   if (loading) {
@@ -14,6 +16,11 @@ const Users = ({ users, loading }) => {
       </div>
     )
   }
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
