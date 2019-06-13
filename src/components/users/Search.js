6d786f4bd514e08f621fd3af5a92d36892fb90f1
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-13 13:24:12 +0100

    Clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ class Search extends Component {
   }
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   }
 
   onSubmit = (e) => {
@@ -21,6 +23,8 @@ class Search extends Component {
   }
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -37,6 +41,7 @@ class Search extends Component {
             className="btn btn-dark btn-block" 
           />
         </form>
+        {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
       </div>
     )
   }
