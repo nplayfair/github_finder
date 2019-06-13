c2d0afb3d6bc9450f98a3385d0330b59efd10a01
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-13 13:35:20 +0100

    Show alert if no string entered in search

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // Search github users
@@ -20,6 +22,13 @@ class App extends Component {
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    // Remove alert after 3 seconds
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  }
   
   render() {
     const { users, loading } = this.state;
@@ -28,8 +37,13 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} 
-            showClear={users.length > 0 ? true : false} />
+          <Alert alert={this.state.alert} />
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+            />
           <Users loading={loading} users={users} />
         </div>
       </div>
